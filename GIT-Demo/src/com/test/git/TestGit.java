c90681bb8118c85c9725733cc722ad66382c60f5
pratap <pratapredyn@gmail.com>
2015-09-12 21:08:45 -0400

    Fix adding 1

--- GIT-Demo/src/com/test/git/TestGit.java
+++ GIT-Demo/src/com/test/git/TestGit.java
@@ -3,7 +3,7 @@ package com.test.git;
 public class TestGit {
 
 	public static void main(String[] args) {
-		System.out.println("Test git");
+		System.out.println("Test git 1");
 		System.out.println("Test git 2");
 		System.out.println("Test git 3");
 		System.out.println("Test git 4");
